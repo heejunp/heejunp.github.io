3f1b408daae9da27a4ab4cf7e7a5e367bc440c37
박희준 <qmzo553@gmail.com>
2025-12-16 15:42:03 +0900

    Design: 문구 추가 및 수정

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "박희준 Blog";
+export const SITE_TITLE = "Welcome to my portfolio";
 export const SITE_DESCRIPTION = "안녕하세요!";
