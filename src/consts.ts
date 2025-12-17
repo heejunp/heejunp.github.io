b01935bea1f85e4c6c4392c26dae0fc19b81280a
박희준 <qmzo553@gmail.com>
2025-12-17 16:27:38 +0900

    Refactor: 컴포넌트 분리

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Welcome to my portfolio";
+export const SITE_TITLE = "Astro";
 export const SITE_DESCRIPTION = "안녕하세요!";
