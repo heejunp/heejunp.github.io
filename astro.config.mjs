696c29b9283cf7ad8d0fb824d7ae84dc51c3422f
parkheejun <qmzo553@gmail.com>
2026-01-13 08:30:13 +0900

    Refactor: 디자인 변경

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://heejunp.github.io",
-    integrations: [sitemap()],
+  site: "https://heejunp.github.io",
+  integrations: [sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
